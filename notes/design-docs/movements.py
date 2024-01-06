5dad6d1e9a4ee85401018774346ad22856df059c
John Desmond <johndesmond631@gmail.com>
2024-01-06 10:53:06 -0500

    we got valid_move workin for white pawn!

--- notes/design-docs/movements.py
+++ notes/design-docs/movements.py
@@ -47,7 +47,7 @@ def get_direction(start_coord: list[int], end_coord: list[int]) -> str:
     if end_coord[1] < start_coord[1] and end_coord[1] == start_coord[1]:
         return 'left'
     
-def in_range(start_coord, end_coord, direction, valid_movements) -> bool:
+def in_range(start_coord, end_coord, direction, valid_movements, move_count) -> bool:
     """
     A rook's movements are valid if it only changes in either row or column.
     i.e. the unbounded multiplier is specific to one location at a time.
@@ -59,7 +59,7 @@ def in_range(start_coord, end_coord, direction, valid_movements) -> bool:
         - end:   [5,0]
         - diff row: -1
         - diff col: 0 
-        - direction_unit: [-1, 0] (up)
+        - unit_direction: [-1, 0] (up)
 
         
     invalid:
@@ -67,26 +67,34 @@ def in_range(start_coord, end_coord, direction, valid_movements) -> bool:
         - end: []
     """
 
-    direction_unit = movements[direction]
+    unit_direction = movements[direction]
     diffs = [end_coord[0] - start_coord[0], end_coord[1] - start_coord[1]]
 
     if valid_movements[direction]['unbounded']:
         pass
     else:
-        return diffs == direction_unit
+        # special case for Pawns' first move
+        # TODO: check that this evaluates correctly for diagonal movements!
+        if move_count == 1:
+            two_up_or_down = [unit_direction[0]*2, unit_direction[1]]
+            return (diffs == unit_direction) or (diffs == two_up_or_down)
+        return diffs == unit_direction
 
-
+"""
+valid_movements and start_coord will be part of the Piece object.
+"""
 def valid_move(
         start_coord: list[int], 
         end_coord: list[int],
         valid_movements: dict,
+        move_count: int = None # in reality, this attribute will only be on Pawns
                ) -> bool:
     """
     color_dir: 1 for Black pawns, -1 for white pawns
     multiplier: if true, you can
     """
     direction = get_direction(start_coord, end_coord)
-    return (direction in valid_movements and in_range(start_coord, end_coord, direction, valid_movements))
+    return (direction in valid_movements and in_range(start_coord, end_coord, direction, valid_movements, move_count))
 
 # aw crap, pawns can move up two only on the first move from their original position
 # gah, encoding that is going to suck.
@@ -98,11 +106,22 @@ white_pawn_movements = {
 
 # white pawns
 # oh snap they can move diagonally if attacking!
-white_pawn_movements = set('up', 'up right', 'up left')
+# white_pawn_movements = set('up', 'up right', 'up left')
 white_pawn_multiplier = 1
 
-
-good_move = valid_move([6,0], [5,0], white_pawn_movements, -1)
+good_move = valid_move([6,0], [5,0], white_pawn_movements, 1)
+print("Should be true: ")
 print(good_move)
 
-invalid_move = valid_move([6,0])
+good_first_move = valid_move([6, 0], [4, 0], white_pawn_movements, 1)
+print("Should be true: ")
+print(good_move)
+
+bad_diagonal_first_move = valid_move([6, 0], [4, 2], white_pawn_movements, 1)
+print("Should be false: ")
+print(bad_diagonal_first_move)
+
+invalid_move = valid_move([6,0], [3,0], white_pawn_movements, 1)
+print("Should be false: ")
+print(invalid_move)
+
--- notes/design-docs/pieces.md
+++ notes/design-docs/pieces.md
@@ -4,6 +4,11 @@ Diagram: https://docs.google.com/drawings/d/1Yl4QXWbwGiyM827-5eQa9BcXNqpx25d9qp-
 I'm not sure how I should instantiate movements because it is sort of the same for each PieceType, but the direction will be different (e.g. Black pawns move down the board, White pawns move up teh board.)
 
 
+For Pawns first movements, they're allowed to go 2 or 1.
+This is only allowed on their first movement.
+Therefore, to Pawns, we need to add an attribute that is move count 
+and we can do a special check for a valid move if 
+
 
 
 ChessPiece:
